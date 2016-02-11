4947367dd8d29a2a93f3691ff21cd665772e4343
kit <ferarbizu@hotmail.com>
2016-02-11 14:11:50 -0600

    Revert "Final"
    This reverts commit ecc3fdd9a396cefb6a1e65be85e45a9357c198d0.

--- PROYECTO2/PROYECTO2/PROYECTO2.cpp
+++ PROYECTO2/PROYECTO2/PROYECTO2.cpp
@@ -1,318 +1,136 @@
+// PROYECTO2.cpp: define el punto de entrada de la aplicación de consola.
+//
+
 #include "stdafx.h"
 #include <iostream>
-
+#include <string>
 using namespace std;
-#define tamanio 10
-#pragma region Proyecto1
-static int Funcion1(int param)
-{
+#define TAMANIO 10//TAMANIO DE LA TABLA
+
+#pragma region FASE1
+static int function(int param) {
 	cout << "Parametro: " << param << " Funcion: 1" << endl;
 	return 1;
 }
-
-class ClassA
+#pragma region CLASES
+class AClass
 {
 public:
-	static int Funcion2(int param);
+
+	static int function(int param) {
+		cout << "Parametro: " << param << " Funcion: 2" << endl;
+		return 2;
+	}
 };
 
-int ClassA::Funcion2(int param)
-{
-	cout << "Parametro: " << param << " Funcion: 2" << endl;
-	return 2;
-}
-
-class ClassB
+class  BClass : public AClass
 {
 public:
-	static void setCallBack(int (ClassB::*&cb)(int));
+	typedef int(BClass::*var)(int);
+	void direc(var &v)
+	{
+		v = &BClass::function;
+	}
+
 private:
-	int Funcion3(int param);
+	int function(int param) {
+		cout << "Parametro: " << param << " Funcion: 3" << endl;
+		return 3;
+	}
 };
-
-int ClassB::Funcion3(int param)
-{
-	cout << "Parametro: " << param << " Funcion: 3" << endl;
-	return 3;
-}
-
-void ClassB::setCallBack(int (ClassB::*&cb)(int))
-{
-	cb = &ClassB::Funcion3;
-}
-
-typedef int(*CallBack)(int);
-typedef int(ClassB::*CallBackB)(int);
 #pragma endregion
-#pragma region proyecto2
-class PCB
+typedef int(*MyCallBack)(int);
+#pragma endregion
+class PCB 
 {
-private:
-	int id;
 public:
-	int Estado, *Funcion;
+	int llave;
+	BClass *k;
+	string estado;
+	PCB(int,string);
+	~PCB();
+private:
 
-	ClassB *objeto;
 
-	PCB(int id, int EstadoParam, int *callback)
-	{
-		this->id = id;
-		this->Estado = EstadoParam;
-		this->Funcion = callback;
-		this->objeto = NULL;
-	}
-
-	PCB(int id, int status, int *callback, ClassB *obj)
-	{
-		this->id = id;
-		this->Estado = status;
-		this->Funcion = callback;
-		this->objeto = obj;
-	}
-
-	~PCB()
-	{
-		this->id = this->Estado = 0;
-		this->Funcion = NULL;
-		if (this->objeto != NULL)
-		{
-			this->objeto = NULL;
-		}
-	}
-	int getId()
-	{
-		return this->id;
-	}
 };
 
-class Kernel
+PCB::PCB(int llave,string est)
 {
+	this->llave = llave;
+	this->estado = est;
+	//this->*k = new BClass;
+}
+PCB::~PCB()
+{
+	this->llave =  NULL;
+	this->estado = "";
+}
+class KERNEL
+{
+public:
+	int contador;
+	PCB *pcb[TAMANIO];
+	KERNEL();
+	~KERNEL();
+	
 private:
-	int id_count;
-	bool Valirdar(int i)
-	{
-		if (i >= tamanio || i < 0)
-			return 3;
-		if (this->pcb[i] != NULL)
+	bool VALIDO(int i) {
+		if (i < NULL || i >= TAMANIO)
 		{
-			if (this->pcb[i]->Estado != 6)
-				return 1;
-			else
-				FinalizarProcesos(i);
+			return -1;
 		}
-		if (count >= tamanio) {
+		if (this->pcb[i] != NULL && (this->pcb[i]->estado != "HECHO"))
+		{
+			return -2;
+		}
+		else if (this->pcb[i] != NULL && !(this->pcb[i]->estado != "HECHO"))
+		{
+			//dotro coso
+			return 9;
+		}
+		if (contador >= TAMANIO)
+		{
 			return 2;
-		}
-		else {
-			return 0;
-		}
-	}
-
-	static void Procesos(PCB *pcb, int val)
-	{
-		int result;
-		int *function = pcb->Funcion;
-		pcb->Estado = 1;
-		if (pcb->objeto != NULL)
-		{
-			CallBackB *cb = (CallBackB*)(function);
-			result = (pcb->objeto->**cb)(val);
 		}
 		else
-		{
-			CallBack cb = (CallBack)*(&function);
-			result = cb(val);
-		}
-		pcb->Estado = 6;
-		cout << "Proceso terminado\nNumero de Proceso: " << pcb->getId() << "\nResultado: " << result << endl << endl;
-	}
-
-	static bool validarElProceso(PCB *current)
-	{
-		if (current == NULL)
-			return 1;
-		if (current->Estado == 6)
-			return 2;
-		return 0;
-	}
-
-	int firstAvailableIndex()
-	{
-		int i = 0;
-		while (i < tamanio)
-		{
-			if (this->pcb[i] == NULL)
-				return i;
-			else if (this->pcb[i]->Estado == 6)
-			{
-				FinalizarProcesos(i);
-				return i;
-			}
-			i++;
-		}
-		return -1;
-	}
-	PCB *getProcesorId(int id)
-	{
-		for (int i = 0; i < tamanio; i++)
-			if (pcb[i] != NULL && pcb[i]->getId() == id) {
-				return pcb[i];
-			}
-		return NULL;
-	}
-
-	int getProceso(int id)
-	{
-		for (int i = 0; i < tamanio; i++)
-			if (pcb[i] != NULL && pcb[i]->getId() == id) {
-				return i;
-			}
-			else {
-				return -1;
-			}
-	}
-public:
-	PCB *pcb[tamanio];
-	int count;
-
-	Kernel()
-	{
-		this->count = this->id_count = 0;
-		for (int i = 0; i < tamanio; i++)
-			this->pcb[i] = NULL;
-	}
-
-	~Kernel()
-	{
-		for (int i = 0; i < tamanio; i++)
-			if (pcb[i] != NULL)
-			{
-				FinalizarProcesos(i);
-				pcb[i] = NULL;
-			}
-		this->count = 0;
-	}
-
-	int CrearProceso(int *callback)
-	{
-		int result = Valirdar(count);
-		if (result != 0)
-			return result;
-		PCB *pcb = new PCB(id_count++, 3, callback);
-		int index = firstAvailableIndex();
-		this->pcb[index] = pcb;
-		count++;
-		return 0;
-	}
-
-	int CrearProceso(int *callback, int i)
-	{
-		int result = Valirdar(i);
-		if (result != 0) {
-			return result;
-		}
-		PCB *pcb = new PCB(id_count++, 3, callback);
-		this->pcb[i] = pcb;
-		count++;
-		return 0;
-	}
-
-	int CrearProceso(int *callback, ClassB *obj)
-	{
-		int result = Valirdar(count);
-		if (result != 0) {
-			return result;
-		}
-		PCB *pcb = new PCB(id_count++, 3, callback, obj);
-		int index = firstAvailableIndex();
-		this->pcb[index] = pcb;
-		count++;
-		return 0;
-	}
-
-	int CrearProceso(int *callback, ClassB *obj, int i)
-	{
-		int result = Valirdar(i);
-		if (result != 0)
-			return result;
-		PCB *pcb = new PCB(id_count++, 3, callback, obj);
-		this->pcb[i] = pcb;
-		i++;
-		return 0;
-	}
-
-	int runProcessAt(int i, int val)
-	{
-		PCB *current = this->pcb[i];
-		int result = validarElProceso(current);
-		if (result != 0) {
-			return result;
-		}
-		Procesos(current, val);
-		return 0;
-	}
-
-	int runProcessById(int id, int val)
-	{
-		PCB *current = getProcesorId(id);
-		int result = validarElProceso(current);
-		if (result != 0) {
-			return result;
-		}
-		Procesos(current, val);
-		return 0;
-	}
-	int ProbarProcesos(int val)
-	{
-		for (int i = 0; i < tamanio; i++)
-		{
-			runProcessAt(i, val);
-			FinalizarProcesos(i);
-		}
-		return 0;
-	}
-
-	int FinalizarProcesos(int i)
-	{
-		if (i >= tamanio || i < 0) {
-			return 3;
-		}
-		if (this->pcb[i] == NULL) {
-			return 1;
-		}
-		count--;
-		this->pcb[i]->~PCB();
-		this->pcb[i] = NULL;
-		return 0;
-	}
-
-	int FinalizarProyectoID(int id)
-	{
-		int index = getProceso(id);
-		if (index < 0) {
-			return 3;
-		}
-		FinalizarProcesos(index);
-		return 0;
+			return 0;
 	}
 };
-#pragma endregion
+
+KERNEL::KERNEL()
+{
+}
+
+KERNEL::~KERNEL()
+{
+}
 int main()
 {
-	int valor1;
-	cout << "Ingrese valor" << endl;
+	//toma de valores
+	int* PCB[10];
+	int valor1, valor2, valor3;
+	cout << "Ingrese valor 1" << endl;
 	cin >> valor1;
-	Kernel *kernel = new Kernel();
-	kernel->CrearProceso((int*)&Funcion1);
-	kernel->CrearProceso((int*)&ClassA::Funcion2);
-	CallBackB cb;
-	ClassB::setCallBack(cb);
-	ClassB *obj = new ClassB();
-	kernel->CrearProceso((int*)&cb, obj);
+	cout << "Ingrese valor 2" << endl;
+	cin >> valor2;
+	cout << "Ingrese valor 3" << endl;
+	cin >> valor3;
 
-	kernel->ProbarProcesos(valor1);
+	MyCallBack m1 = NULL, m2 = NULL;
+	m1 = &function;
+	m2 = &AClass::function;
+	BClass* b = new  BClass;
+	BClass::var m = NULL;
 
+	PCB[0] = (int*)&m1;
+	PCB[1] = (int*)&m2;
+	PCB[2] = (int*)&m;
+
+	b->direc(m);
+	m1(valor1);
+
+	m2(valor2);
+	(b->*m)(valor3);
 	system("PAUSE...");
-	kernel->~Kernel();
 	return 0;
 }
