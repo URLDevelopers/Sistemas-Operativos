0a282e9c49fc288a311182d36f252d69f6ca3016
Francisco Manjón Gottlob <franciscogottlob@hotmail.com>
2016-03-17 16:56:19 -0600

    Update Worker

--- Worker/Worker/Program.cs
+++ Worker/Worker/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Net.Sockets;
 
 namespace Worker
 {
@@ -11,6 +12,7 @@ namespace Worker
     {
         static void Main(string[] args)
         {
+            /*
             Worker workerobject = new Worker();
             Thread workerThread = new Thread(workerobject.DoWork);
 
@@ -22,6 +24,44 @@ namespace Worker
             workerobject.RequestStop();
             workerThread.Join();
             Console.WriteLine("main thread: Worker thread has terminated.");
+            Console.ReadLine();
+            */
+
+            TcpListener serverSocket = new TcpListener(2439);
+            int requestCount = 0;
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" >> Server Started");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine(" >> Accept connection from client");
+            requestCount = 0;
+            while ((true))
+            {
+                try
+                {
+                    requestCount++;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[65536];
+                    
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize); //aqui truena
+                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> Data from client - " + dataFromClient);
+                    string serverResponse = "A creeper is behind you o.O";
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("tronitos");
+                    break;
+                }
+            }
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> exit");
             Console.ReadLine();
         }
     }
